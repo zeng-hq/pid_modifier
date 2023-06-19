8101f6d61d428f088dfd0076dd616713461b73a4
Haoqi Zeng <zeng-hq@sjtu.edu.cn>
2023-06-19 15:47:34 +0800

    node init

---
+++ pid_modifier/pidmodify_node.py
@@ -0,0 +1,9 @@
+def main():
+    print('Hi from pid_modifier.')
+    while True:
+        s = input("input:")
+        print("get:"+s)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'pid_modifier'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Haoqi Zeng',
+    maintainer_email='zeng-hq@sjtu.edu.cn',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pidmodify_node = pid_modifier.pidmodify_node:main'
+        ],
+    },
+)
